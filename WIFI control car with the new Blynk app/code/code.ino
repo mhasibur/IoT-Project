6cf2eecc5a7101c58ae294b68f7e92516d7fd6f0
Hasibur Rahman <73151150+mhasibur@users.noreply.github.com>
2024-06-09 18:26:18 +0600

    Add files via upload

---
+++ WIFI control car with the new Blynk app/code/code.ino
@@ -0,0 +1,122 @@
+/*Nodemcu ESP8266 WIFI control car with the New Blynk app.
+   This code created by the SriTu Hobby team.
+   Home Page
+*/
+#define BLYNK_TEMPLATE_ID "TMPL69DMPRIuV"
+#define BLYNK_TEMPLATE_NAME "Smart Car"
+//#define BLYNK_AUTH_TOKEN "QQr4ZthYeuEBM75jl0alDqx684Z4kBfx"
+// Include the library files
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+
+
+// Define the motor pins
+#define ENA D0
+#define IN1 D1
+#define IN2 D2
+#define IN3 D3
+#define IN4 D4
+#define ENB D5
+
+// Variables for the Blynk widget values
+int x = 50;
+int y = 50;
+int Speed;
+
+char auth[] = "75TTbutlsoqYKMHr0n9Ce1pDp_E53pDO"; //Enter your Blynk auth token
+char ssid[] = "Md..."; //Enter your WIFI name
+char pass[] = "md......"; //Enter your WIFI passowrd
+
+
+void setup() {
+  Serial.begin(9600);
+  //Set the motor pins as output pins
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  // Initialize the Blynk library
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+}
+
+// Get the joystick values
+BLYNK_WRITE(V0) {
+  x = param[0].asInt();
+}
+// Get the joystick values
+BLYNK_WRITE(V1) {
+  y = param[0].asInt();
+}
+//Get the slider values
+BLYNK_WRITE(V2) {
+  Speed = param.asInt();
+}
+
+// Check these values using the IF condition
+void smartcar() {
+  if (y > 70) {
+    carForward();
+    Serial.println("carForward");
+  } else if (y < 30) {
+    carBackward();
+    Serial.println("carBackward");
+  } else if (x < 30) {
+    carLeft();
+    Serial.println("carLeft");
+  } else if (x > 70) {
+    carRight();
+    Serial.println("carRight");
+  } else if (x < 70 && x > 30 && y < 70 && y > 30) {
+    carStop();
+    Serial.println("carstop");
+  }
+}
+void loop() {
+  Blynk.run();// Run the blynk function
+  smartcar();// Call the main function
+}
+
+/**************Motor movement functions*****************/
+void carForward() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carBackward() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void carLeft() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+void carRight() {
+  analogWrite(ENA, Speed);
+  analogWrite(ENB, Speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+void carStop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
